1b94edb37e6a6ab35df320288e752450924e259f
jaebriantes-dev <jaebriantes@gmail.com>
2025-10-21 12:39:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -423,9 +423,10 @@ window.__TaskHero={tasks,saveAll,loadAll,gainXP};
 
 // === Service Worker Registration ===
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("service-worker.js")
+  navigator.serviceWorker.register('./service-worker.js')
     .then(() => console.log("✅ Service Worker registered"))
     .catch(err => console.log("❌ Service Worker failed:", err));
 }
+
 
 
